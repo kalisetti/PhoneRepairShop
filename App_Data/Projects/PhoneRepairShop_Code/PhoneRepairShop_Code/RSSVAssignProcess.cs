0bbecc0b37e80b68edf5e34f2a566baab71f77b3
siva-alphabiz <siva@alphabiz.com.au>
2024-05-29 13:49:31 +0800

    T240 - Step 1.1.1: Creating the Form --Self-Guided Exercise

---
+++ App_Data/Projects/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVAssignProcess.cs
@@ -0,0 +1,26 @@
+using System;
+using PX.Data;
+
+namespace PhoneRepairShop {
+	public class RSSVAssignProcess : PXGraph<RSSVAssignProcess> {
+
+		//public PXSave<MasterTable> Save;
+		//public PXCancel<MasterTable> Cancel;
+
+
+		//public PXFilter<MasterTable> MasterView;
+		//public PXFilter<DetailsTable> DetailsView;
+
+		//[Serializable]
+		//public class MasterTable : IBqlTable {
+
+		//}
+
+		//[Serializable]
+		//public class DetailsTable : IBqlTable {
+
+		//}
+
+
+	}
+}
