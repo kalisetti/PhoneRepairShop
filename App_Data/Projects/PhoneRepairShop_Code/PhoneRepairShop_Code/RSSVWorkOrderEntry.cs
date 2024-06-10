229d8f3124997c5afbcebe1fa354f6566b5073fc
siva-alphabiz <siva@alphabiz.com.au>
2024-06-10 14:35:20 +0800

    T250 - Lesson 1.1: Configure the Inquiry Form

--- App_Data/Projects/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVWorkOrderEntry.cs
+++ App_Data/Projects/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVWorkOrderEntry.cs
@@ -14,7 +14,7 @@ namespace PhoneRepairShop {
 		// The view for the auto-numbering of records
 		public PXSetup<RSSVSetup> AutoNumSetup;
 
-		// The graph constructor
+		// The graph constructor 
 		public RSSVWorkOrderEntry() {
 			RSSVSetup setup = AutoNumSetup.Current;
 		}
